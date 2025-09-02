9eb1190a93c4635490a97ba16988a21f0e70dda8
akanomdeplume <gyan.alandia@ucas-edu.net>
2025-09-02 14:38:49 -0600

    Update with variables

---
+++ practices/Variables.py
@@ -0,0 +1,11 @@
+#GA P7 Variables Practice
+your_name="Gyan Alandia"
+number_1-10="7" \
+school_name="UCAS"
+the_year="2025'
+number_1-1000="894"
+eye_color="brown"
+ate_for_breakfast="bagel"
+age="14"
+favorite_color="dark red"
+favorite_subject="lunch"
