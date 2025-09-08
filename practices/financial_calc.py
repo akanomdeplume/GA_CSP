4fc506da23a8e36b36bd75de609de06ac458557e
akanomdeplume <gyan.alandia@ucas-edu.net>
2025-09-08 19:53:35 +0000

    I did the financial calc

--- practices/financial_calc.py
+++ practices/financial_calc.py
@@ -7,21 +7,16 @@ utilities = float(input("What is your monthly utilities: "))
 groceries = float(input("What is your monthly groceries: "))
 transportation = float(input("What is your monthly transportation: "))
 
-# percentages
 rent_pct = (rent / income) * 100
 utilities_pct = (utilities / income) * 100
 groceries_pct = (groceries / income) * 100
 transport_pct = (transportation / income) * 100
 
-# savings (10%)
-savings = income * 0.10
 savings_pct = 10
 
-# leftover
 total_expenses = rent + utilities + groceries + transportation + savings
 leftover = income - total_expenses
 
-# print results
 print("Your rent is $", rent, "and that is", round(rent_pct), "% of your income.")
 print("Your utilities are $", utilities, "and that is", round(utilities_pct), "% of your income.")
 print("Your groceries are $", groceries, "and that is", round(groceries_pct), "% of your income.")
