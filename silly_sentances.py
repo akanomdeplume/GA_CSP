50ca1def47e76b428d85223ae7c9ffff0d22a050
akanomdeplume <gyan.alandia@ucas-edu.net>
2025-09-15 14:05:15 -0600

    silly

---
+++ silly_sentances.py
@@ -0,0 +1,8 @@
+#GA 7th Silly sentences
+name = input("give me a name:\n").strip().title()
+
+verb = input("give me a past tense verb:\n")
+
+number = input("give me a any number:\n")
+
+print(name + + verb + + "from" + + number + + "trees.")
