f5b07c5a11268d55bdfe91d5747d6ad22ad8f090
akanomdeplume <gyan.alandia@ucas-edu.net>
2025-09-17 14:41:46 -0600

    rhitnhg

---
+++ Notes/Conditionals.py
@@ -0,0 +1,14 @@
+#GA 7th Condition Note
+
+homework_done = input("is your homework done?:").strip().capitalize
+
+if homework_done == "Yes"
+    print("Yes you can go")
+else:
+    print("then go do your homework!")
+
+    grade = 100
+    if grade > 89:
+        print(GOOD JOB! IM SO PROUD OF YOU!!)
+else:
+    print(I HATE YOU YOURE A FAILURE!)
