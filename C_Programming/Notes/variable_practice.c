0e5e38a8091c4791dfa31174e65beef2d4139020
akanomdeplume <gyan.alandia@ucas-edu.net>
2025-10-09 14:25:59 -0600

    string

--- C_Programming/Notes/variable_practice.c
+++ C_Programming/Notes/variable_practice.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 
 int main(void){
-    char name = "Gyan Alandia";
-    char school = "Utah County Academy of Sciences";
+    char name[] = "Gyan Alandia";
+    char school[] = "Utah County Academy of Sciences";
     int number = 6;
     int year = 2025;
     float gpa = 3.2;
-    char eye = " Brown";
+    char eye[] = "Brown";
     char breakfast = "yogurt";
     int age = 15;
-    char color = "Dark Red";
-    char subject = "World Civilizations";
-    printf("My name is %c", name);
-    printf("I go to %c", school);
-    printf("A number between 1 and 10 is %d", number);
-    printf("It is %d currently", year);
-    printf("A GPA is %f", gpa);
-    printf("My eye color is a %c", eye);
-    printf("I ate %c for breakfast today", breakfast);
-    printf("I am %d years old", age);
-    printf("My favorite color is %c", color);
-    printf("My favorite subject in school is %c", subject);
+    char color[] = "Dark Red";
+    char subject[] = "World Civilizations";
+    printf("My name is %s\n", name);
+    printf("I go to %s\n", school);
+    printf("A number between 1 and 10 is %d\n", number);
+    printf("It is %d currently\n", year);
+    printf("A GPA is %f\n", gpa);
+    printf("My eye color is a %s\n", eye);
+    printf("I ate %s for breakfast today\n", breakfast);
+    printf("I am %d years old\n", age);
+    printf("My favorite color is %c\n", color);
+    printf("My favorite subject in school is %c\n", subject);
 }
